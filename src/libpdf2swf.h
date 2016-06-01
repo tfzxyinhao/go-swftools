186685837b6893521d7faf1a4d9bcb47307846ae
yinh <yinhao@dooland.com>
2016-06-01 15:57:30 +0800

    change function sign

--- src/libpdf2swf.h
+++ src/libpdf2swf.h
@@ -1,1 +1,11 @@
-int convert(int argc,char* argv[]);
+enum ErrorCode
+{
+	NOERROR,
+	PARAM_ERROR,
+	FILE_NOT_EXISTS,
+	FILE_OPEN_ERROR,
+	RANGE_ERROR,
+	SAVE_ERROR
+};
+
+ErrorCode convert(const char* filename, int page, const char* out, const char* password = 0);
